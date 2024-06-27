d1c41ec18a2af1f859df725cec05e2c9f0232150
sh20raj <shaswatraj3@gmail.com>
2024-06-27 07:35:53 +0530

    Refactor client-side fetch and error handling in new2/page.js
    This commit refactors the client-side fetch implementation in the `new2/page.js` file. It replaces the previous usage of the `fetch` function with the `fetch` API and adds error handling for network errors. Additionally, it improves the code readability by adding proper indentation and line breaks.

--- src/app/new2/page.js
+++ src/app/new2/page.js
@@ -1,26 +1,38 @@
-'use client'
-//this is app/new2/page.js and it will not catch the client side fetch
-// It usage react hooks to fetch data
- 
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
- 
-export default function Page() {
-  const [ data, setData ] = useState({})
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/posts/1`)
-      .then((response) => response.json())
-      .then((json) => {
-        setData(json)
-      })
+'use client';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
-  },[])
- 
-  return <>
-  <Link href={"/"}>Main</Link>
-  <br />
-  data.title {data.title}
-  
-  
-  </>
+export default function Page() {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((json) => {
+        setData(json);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return 'Loading...';
+  if (error) return `Error: ${error.message}`;
+
+  return (
+    <>
+      <Link href="/">Main</Link>
+      <br />
+      <div>Data Title: {data.title}</div>
+    </>
+  );
 }
